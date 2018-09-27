f8f1bbce6c108e9f2e6f500112c2c61180308392
lmahimer <2015081538@ust-ics.mygbiz.com>
2018-09-27 15:40:08 +0800

    Initial commit

---
+++ app/src/main/java/com/mahimer/leania/mahimerleanialab5/MainActivity.java
@@ -0,0 +1,31 @@
+package com.mahimer.leania.mahimerleanialab5;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent i = new Intent(this, CustomService.class);
+        this.startService(i);
+    }
+
+    public void process (View v){
+        Intent i=null, chooser=null;
+        if (v.getId() == R.id.btn2){
+            i = new Intent(this, SecondActivity.class);
+            startActivity(i);
+        } else if (v.getId() == R.id.btnMap){
+            i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse("geo:14.5792,120.9723"));
+            chooser = Intent.createChooser(i, "Select application");
+            startActivity(chooser);
+        }
+    }
+}
---
+++ app/src/main/java/com/mahimer/leania/mahimerleanialab5/SecondActivity.java
@@ -0,0 +1,29 @@
+package com.mahimer.leania.mahimerleanialab5;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    public void process (View v){
+        Intent i=null, chooser=null;
+        if (v.getId() == R.id.btn1){
+            i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        } else if (v.getId() == R.id.btnMap1){
+            i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse("geo:14.6175,120.9874"));
+            chooser = Intent.createChooser(i, "Select application");
+            startActivity(chooser);
+        }
+    }
+}
